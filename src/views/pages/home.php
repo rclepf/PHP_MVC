552a6410bcc645930887c8cb491065a0506e8ddc
Rodolfo Clepf <rclepf@gmail.com>
2023-02-08 16:22:32 -0300

    MVCInicial

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,3 +1,19 @@
 <?php $render('header'); ?>
 
-Opa, <?=$nome;?>
+<hr>
+
+Meu nome é
+<?php echo $nome; ?> <br>
+Minha idade é
+<?php echo $idade; ?> <br>
+
+<hr>
+    
+<?php foreach ($posts as $post): ?>
+    <h3>
+        <?php echo $post['titulo']; ?>
+    </h3>
+    <p>
+        <?php echo $post['corpo']; ?>
+    </p>
+<?php endforeach; ?>
