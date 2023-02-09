20e2d48d77326cdb8a78c54a97b0325d5d841151
Rodolfo Clepf <rclepf@gmail.com>
2023-02-09 12:24:17 -0300

    CRUD MVC UPDATE

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -18,24 +18,42 @@ class UsuariosController extends Controller
 
         if ($name && $email) {
             $data = Usuario::select()->where('email', $email)->execute();
-            if (count($data) === 0) {                
+            if (count($data) === 0) {
                 Usuario::insert([
                     'name' => $name,
                     'email' => $email
                 ])->execute();
                 $this->redirect('/');
             }
-        }        
+        }
         $this->redirect('/novo');
     }
 
     public function edit($args)
     {
-        
+        $usuario = Usuario::select()->find($args['id']);
+
+        $this->render('edit', ['usuario' => $usuario]);
+    }
+
+    public function editAction($args)
+    {
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
+
+        if ($name && $email) {
+            Usuario::update()
+                ->set('name', $name)
+                ->set('email', $email)
+                ->where('id', $args['id'])
+                ->execute();
+            $this->redirect('/');
+        }
+        $this->redirect('/usuario/' . $args['id'] . '/editar');
     }
 
     public function del($args)
     {
-        
+
     }
 }
